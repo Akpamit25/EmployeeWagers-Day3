f431c00cfb701a175417ba038646a92a0b07eb1e
Amit Kumar <amitnitsri25@gmail.com>
2020-10-19 15:44:52 +0530

    Welcome To Employee Wage Computation Program

---
+++ src/com/capgemini/EmployeeWage/EmployeeWage.java
@@ -0,0 +1,10 @@
+package com.capgemini.EmployeeWage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcom To Employee Wage Computation Program On Master Branch");
+	}
+
+}
