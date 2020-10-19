f5129554517465f552592dfa29eeee48aea65d53
Amit Kumar <amitnitsri25@gmail.com>
2020-10-19 21:03:45 +0530

    UC6 : Wages Only Till When Monthly working hours of 100 is reached or maximum 20 days of work in a month

--- src/com/capgemini/EmployeeWage/EmployeeWage.java
+++ src/com/capgemini/EmployeeWage/EmployeeWage.java
@@ -2,6 +2,7 @@ package com.capgemini.EmployeeWage;
 
 public class EmployeeWage {
 	static int empWage_per_hour = 20;
+	//static int empHour = 0;
 	static int working_days = 20;
 	static int totalEmployeeWage = 0;
 	static int totalWorkingHour = 0;
@@ -10,32 +11,30 @@ public class EmployeeWage {
 
 		EmployeeWage empWage = new EmployeeWage();
 		empWage.checkEmployee(); // checks whether employee is present or not
-		System.out.println("Total Employee Wage = " + totalEmployeeWage);
+		System.out.println("\n Total Employee Wage = " + totalEmployeeWage);
+		System.out.println("\n Total Working Hour : "+totalWorkingHour);
 	}
 
 	public void checkEmployee() {
-		int empHour = 0;
+		int empHour =0;
 		for (int day = 1; day <= working_days; day++) {
 			if(totalWorkingHour <= 100) {
 				int x = (int) (Math.random() * 3);
 				switch (x) { // Using Switch Case
 				case 0:
-					System.out.println("Day "+day+":");
-					System.out.println("Employee Is Absent");
+					System.out.println("\n Employee Is Absent");
 					empHour = 0;
 					dailyEmployeeWage(empHour);
 					break;
 
 				case 1:
-					System.out.println("Day "+day+":");
-					System.out.println("Employee Is Present");
+					System.out.println("\n Employee Is Present");
 					empHour = 8;
 					dailyEmployeeWage(empHour);
 					break;
 
 				case 2:
-					System.out.println("Day "+day+":");
-					System.out.println("Part-Time Employee");
+					System.out.println("\n Part-Time Employee");
 					empHour = 4;
 					dailyEmployeeWage(empHour);
 					break;
@@ -45,15 +44,19 @@ public class EmployeeWage {
 				 * dailyEmployeeWage(empHour); break;
 				 */
 				}
+				System.out.print("Day No. :"+day+"\n");
 		}
 		}
 	}
 
 	public void dailyEmployeeWage(int hour) {
-		int wage = hour * empWage_per_hour;
-		System.out.println("Employee's Daily Wage = " + wage+ "\n");
-		totalEmployeeWage += wage;
 		totalWorkingHour += hour;
+		System.out.print("Employee Hour = "+totalWorkingHour+ ",  ");
+		int wage = hour * empWage_per_hour;
+		//System.out.println("Employee's Daily Wage = " + wage);
+		totalEmployeeWage += wage;
+		//totalWorkingHour += hour;
+
 	}
 
 }
